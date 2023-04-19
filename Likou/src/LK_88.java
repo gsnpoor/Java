08f3ffd0354df1a12f2b3451cfabc4f7bdd75ee9
gsnpoor <2366106257@qq.com>
2023-04-19 10:51:41 +0800

    2023.4.19 更新LK

---
+++ Likou/src/LK_88.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class LK_88 {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        merge(nums1, 3, nums2, 3);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m; i < nums1.length; ++i) {
+            nums1[i] = nums2[i - m];
+        }
+        Arrays.sort(nums1);
+    }
+}
