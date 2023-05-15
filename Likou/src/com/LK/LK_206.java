8b3232b66f0e5b9c00ca38d5fb6fcc2c3bae1ebf
gsnpoor <2366106257@qq.com>
2023-05-15 20:44:57 +0800

    2023.5.13 更新LK

---
+++ Likou/src/com/LK/LK_206.java
@@ -0,0 +1,43 @@
+package com.LK;
+
+public class LK_206 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //迭代
+    /*public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }*/
+    //递归
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
